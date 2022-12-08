97dfb999f0387d21dce4ccc78a173c978c07b3e8
CloudFlavorKettle <ekaqh8624@gmail.com>
2022-12-08 11:43:32 +0900

    django course 39 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
