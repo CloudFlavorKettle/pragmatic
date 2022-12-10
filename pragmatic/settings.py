726cb5cc5a93860f46e8e963fcf21289906c2280
CloudFlavorKettle <ekaqh8624@gmail.com>
2022-12-10 13:59:03 +0900

    django course 40 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
 ]
 
 MIDDLEWARE = [
@@ -135,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
